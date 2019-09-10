8cbfdcbfc27c1e67d21cb63812d870c46e76a603
gissandrogama <gissandrogama@gmail.com>
2019-09-10 15:26:40 -0300

    Cadastro de Ordem de serviço

--- src/pages/CadastroOrdem.js
+++ src/pages/CadastroOrdem.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react'
 
+import api from '../services/api'
+
+import './CadOrdem.css'
+
 class CadOrdem extends Component {
+    state = {
+        equipamento: '',
+        setor: '',
+        data: '',
+        descricao: '',
+        solicitante: '',
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault()
+        
+        await api.post('/ordem', {
+            
+        })
+
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
     render(){
         return(
-            <h1>Cadastro de OS</h1>
+            <form id="new-os" onSubmit={this.handleSubmit}>
+                <input 
+                    type="text"
+                    name="equipamento"
+                    placeholder="tipo de equipamento"
+                    onChange={this.handleChange}
+                    value={this.state.equipamento}
+                />
+                <input 
+                    type="text"
+                    name="setor"
+                    placeholder="Qual setor"
+                    onChange={this.handleChange}
+                    value={this.state.setor}
+                />
+                <input 
+                    type="text"
+                    name="data"
+                    placeholder="data de cadastro da os"
+                    onChange={this.handleChange}
+                    value={this.state.data}
+                />
+                <textarea
+                    type="text"
+                    rows="7"
+                    cols="30"     
+                    name="descricao"
+                    placeholder="descrição do problema ..."
+                    onChange={this.handleChange}
+                    value={this.state.descricao}
+                />
+                <input 
+                    type="text"
+                    name="solicitante"
+                    placeholder="Solicitante"
+                    onChange={this.handleChange}
+                    value={this.state.solicitante}
+                />
+
+                <button type="submit">Cadastrar</button>
+            </form>
         )
     }
 }
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+import aixos from 'aixos'
+
+const api = aixos.create({ baseURL: 'http://localhost:3333/jmapi'})
+
+export default api 
