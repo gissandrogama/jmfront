866b5025a619d765371d74ebb4b601d7c14ad1a4
gissandrogama <gissandrogama@gmail.com>
2019-09-12 14:56:37 -0300

    lista ordens

--- src/pages/Ordems.js
+++ src/pages/Ordems.js
@@ -4,7 +4,13 @@ class Ordems extends Component {
     render(){
         return(
             <div id="lista-ordem">
-                
+                <ul>
+                    <li>Nome</li>
+                    <li>Data</li>
+                </ul>
+                <div>
+                    <button>Atender</button>
+                </div>
             </div>
         )
     }
