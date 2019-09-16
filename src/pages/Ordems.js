dbb12b4235c0b14fdff3ad1dd0c0a5fb8465a00e
gissandrogama <gissandrogama@gmail.com>
2019-09-16 18:17:47 -0300

    lista de os cadastradas

--- src/pages/Ordems.js
+++ src/pages/Ordems.js
@@ -3,17 +3,27 @@ import React, { Component } from 'react'
 import './Ordem.css'
 
 class Ordems extends Component {
-    render(){
-        return(
-            <div id="lista-ordem">
-                <ul>
-                    <li>Nome</li>
-                    <li>Data</li>
-                </ul>
-                <div>
-                    <button>Atender</button>
-                </div>
-            </div>
+    render() {
+        return ( 
+            <table>           
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Data</th>
+                        <th>ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Trator</td>
+                        <td>22/08/2019</td>
+                        <td>
+                            <button>Atender</button>
+                            <button>Visualizar</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>            
         )
     }
 }
