e346eef969be509acf3dba315167684e86cb67c1
gissandrogama <gissandrogama@gmail.com>
2019-09-24 01:00:16 -0300

    ajuste visualeze

--- src/pages/Visualize.js
+++ src/pages/Visualize.js
@@ -14,16 +14,45 @@ export default class Visualize extends Component {
         data_atend: '',
         status: '',
     }
-    
-    render(){
-        return(
+
+    async componentDidMount() {
+
+        const { id } = this.props.match.params
+
+        const response = await api.get(`/ordem/${id}`)
+
+        this.setState({
+            equipamento: response.data.equipamento,
+            setor: response.data.setor,
+            data: response.data.data,
+            descricao: response.data.descricao,
+            solicitante: response.data.solicitante,
+            data_atend: response.data.data_atend,
+            status: response.data.status,
+        })
+    }
+
+    render() {
+        return (
             <div className="visual">
                 <article>
                     <label><strong>Equipamento: </strong></label>
                     <p>Trator</p>
-                    <label><strong>Data de Abertura</strong></label>
-                    
-
+                    <label><strong>Data de Abertura:</strong></label>
+                    <p>22/09/2019</p>
+                    <label><strong>Descrição:</strong></label>
+                    <p>Troca de caixa de marcha</p>
+                    <label><strong>Solicitante:</strong></label>
+                    <p>João</p>
+                    <form id="new_ordem">
+                        <label><strong>Data de atendimento:</strong></label>
+                        <input
+                            type="date"
+                            name="data_atend"
+                            onChange={this.handleChange}
+                            value={this.state.url}
+                        />
+                    </form>
                 </article>
             </div>
         )
