a12861b02569ac263f7599f0dd026ad9a4538cf8
gissandrogama <gissandrogama@gmail.com>
2019-09-27 13:28:31 -0300

    construindo Menu

---
+++ src/components/MenuItem.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+import './MenuIntem.css'
+
+class MenuIntem extends Component {
+    render(){
+        <div className="menu">
+            <h6>teste</h6>
+        </div>
+    }
+}
+
+MenuIntem.prototype = {
+    closeCallback: React.prototype.func.isRequired
+}
+
+export default MenuIntem
