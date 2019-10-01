ba238660a3699b8ced00e4aad20c7f233c5a59fc
gissandrogama <gissandrogama@gmail.com>
2019-09-30 22:16:39 -0300

    ajuste de margem

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,8 @@ export default function Header() {
       <nav className="menu_navigation">
         <div></div>
         <div className="menu_logo"><img src={logo} alt="logo JM Serviços" /></div>
-        <div className="meu_navigation_items">
+        <div className="spacer" />
+        <div className="menu_navigation_items">
           <ul>
             <li><Link to={`/`}>Ordem de Serviços</Link></li>
             <li><Link to={`/new`}>Cadastro</Link></li>
