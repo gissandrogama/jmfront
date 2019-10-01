75b26b32f2342b4587daf7bce8ba3ca86e74539a
gissandrogama <gissandrogama@gmail.com>
2019-09-30 23:44:50 -0300

    sidedrawer e backdrop ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
+import Backdrop from './components/Backdrop/Backdrop'
 import SideDrawer from './components/SideDrawer/SideDrawer'
 import Header from './components/Header'
 import Routes from './routes'
@@ -8,15 +9,16 @@ import Routes from './routes'
 
 function App() {
   return (
-    <div style={{height: '100%'}}>      
     <BrowserRouter>
+    <div style={{height: '100%'}}>      
       <Header />
       <SideDrawer />
+      <Backdrop />
       <main style={{marginTop: '64px'}}>
         <Routes />
       </main>
-    </BrowserRouter>
     </div>
+    </BrowserRouter>
   );
 }
 
